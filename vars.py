61405522ebc7bbf9719f54711e3391403d255f7f
vishalkumar27440 <vishalkumar27440@gmail.com>
2026-08-04 20:01:58 +0530

    vars.py
    Updated API_ID, API_HASH, and BOT_TOKEN with default values.

--- vars.py
+++ vars.py
@@ -1,6 +1,6 @@
 # DON'T add anything here just add in render's secret or env section 
 from os import environ
 
-API_ID = int(environ.get("API_ID", ""))
-API_HASH = environ.get("API_HASH", "")
-BOT_TOKEN = environ.get("BOT_TOKEN", "")
+API_ID = int(environ.get("API_ID", "21595709"))
+API_HASH = environ.get("API_HASH", "6b683b86a90c6fae0fbe50a6494bdd53")
+BOT_TOKEN = environ.get("BOT_TOKEN", "8972420117:AAGTSEwfrv3U8utKnB1vW7iIZ_gYV4rkhro")
